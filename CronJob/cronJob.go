20145bab6143999539ae1848ccb29bbc391b1caa
chaos-star <83060420+chaos-star@users.noreply.github.com>
2023-10-10 17:48:55 +0800

    optimize cron join job

--- CronJob/cronJob.go
+++ CronJob/cronJob.go
@@ -3,6 +3,7 @@ package CronJob
 import "github.com/robfig/cron/v3"
 
 type CronJob struct {
+	jobs map[string]SpecJob
 	*cron.Cron
 }
 
@@ -12,13 +13,22 @@ type SpecJob struct {
 }
 
 func (cj *CronJob) JoinJobs(jobs map[string]SpecJob) {
+	cj.jobs = jobs
 	for _, item := range jobs {
 		cj.AddJob(item.Spec, item.Cmd)
 	}
 }
+func (cj *CronJob) GetJob(name string) *SpecJob {
+	var pJob *SpecJob
+	if job, ok := cj.jobs[name]; ok {
+		pJob = &job
+	}
+	return pJob
+}
 
 func Initialize() *CronJob {
 	return &CronJob{
+		make(map[string]SpecJob),
 		cron.New(),
 	}
 }
